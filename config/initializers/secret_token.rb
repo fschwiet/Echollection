e07f957780a471946f60457618ee6434306a9e38
fschwiet <fschwiet@gmail.com>
2012-06-23 15:34:58 -0700

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echollection::Application.config.secret_token = '37e59707c97a52e8ba0dedad59edf05b34f9146591c97fdb7b488a0901f9b90a16f8cd93885526298a7e43053dc891dbf8b603cbf03991cc44e568f20f945ae9'
