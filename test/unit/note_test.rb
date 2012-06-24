c1b9a63bd89553d4df5787816e1c0c62ffd50789
fschwiet <fschwiet@gmail.com>
2012-06-23 18:18:29 -0700

    add a validation test, and a test showing how to load a predefined element from fixture

--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a title" do
+  	note = Note.new;
+
+    assert note.invalid?
+    assert note.errors[:title].any?
+    
+    note.title = "hi"
+
+    assert note.valid?
+  end
+
+  test "example loading note from fixture" do
+  	puts(notes(:one).title)
+  end
+
 end
