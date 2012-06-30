13c620a942308fea1ef2e9bb14f7cc08825231e3
fschwiet <fschwiet@gmail.com>
2012-06-29 17:09:49 -0700

    continuing login test...

--- db/migrate/20120629213739_create_users.rb
+++ db/migrate/20120629213739_create_users.rb
@@ -6,8 +6,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_salt,       :null => false
       t.string :email,               :null => false
       t.string :persistence_token,   :null => false
-      t.string :single_access_token, :null => false
-      t.string :perishable_token,    :null => false                   
 
       t.integer :login_count,         :null => false, :default => 0
       t.integer :failed_login_count,  :null => false, :default => 0
--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
   test "can create a new account" do
-  	get :new
-  	assert_select "input[name='user_session[email]']"
-  	assert_select "input[name='user_session[password]']"
+    get :new
+    assert_select "input[name='user_session[email]']"
+    assert_select "input[name='user_session[password]']"
+
+    post :create, :user_session => { email: 'bjohnson@server.com', password: 'benrock'}
+    assert user_session = UserSession.find
+    assert_equal users(:ben), user_session.user
+    assert_redirected_to :root      
   end
 end
