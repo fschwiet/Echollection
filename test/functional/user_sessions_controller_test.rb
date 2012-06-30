f0b0d70a8cc9c340665802494ac9fde778582928
fschwiet <fschwiet@gmail.com>
2012-06-29 17:25:22 -0700

    can log in

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  test "can create a new account" do
+  test "should be able to login" do
     get :new
     assert_select "input[name='user_session[email]']"
     assert_select "input[name='user_session[password]']"
 
-    post :create, :user_session => { email: 'bjohnson@server.com', password: 'benrock'}
-    assert user_session = UserSession.find
-    assert_equal users(:ben), user_session.user
+    post :create, :user_session => { email: 'bjohnson@server.com', password: 'benrocks'}
     assert_redirected_to :root      
+    assert user_session = UserSession.find, "Should have saved user session"
+    assert_equal users(:ben), user_session.user, "Should have associated user with session"
   end
+
+  test "should receive error when using invalid username" do
+  end
+
 end
