812caa100746934d0e6063b94b518f5c959d6071
fschwiet <fschwiet@gmail.com>
2012-07-01 12:48:52 -0700

    user registration test

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -10,18 +10,31 @@ def assert_at_register_form
 end
 
 class UsersControllerTest < ActionController::TestCase
+
+  SOME_PASSWORD = 'accuallyWasDolan'
+
   test "should be able to create an account" do
     get :new
     assert_at_register_form
 
-    @password = 'accuallyWasDolan'
+    @password = SOME_PASSWORD
 
     post :create, :user => { 
       name: 'gooby', 
       email: 'gooby@please.com', 
-      password: @password, 
-      password_confirmation: @password}   
+      password: SOME_PASSWORD, 
+      password_confirmation: SOME_PASSWORD}   
       
     should_be_logged_in_as 'gooby'
+  end
+
+  test "should receive error when registration form is not filled correctly" do
+    post :create, :user => { 
+      name: 'gooby', 
+      email: 'gooby', 
+      password: SOME_PASSWORD, 
+      password_confirmation: SOME_PASSWORD}   
+
+    assert_select "div#error_explanation", "Email should look like an email address."
   end
 end
